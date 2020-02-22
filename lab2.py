a23fbb93d04ba0ff90622d2ac1724e93b6d686bc
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-02-22 10:29:25 +0200

    Lab2 finished

--- lab2.py
+++ lab2.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Optional
+from typing import Optional
 from numpy import array, log2, where, ndarray, sum
 
 
@@ -15,9 +15,18 @@ def main():
         [1 / 16, 1 / 16, 1 / 16, 1 / 16],
         [1 / 4, 0.0, 0.0, 0.0]
     ])
-    print(f'H(A) = {entropy(p, 1)}')
-    print(f'H(B) = {entropy(p, 0)}')
-    print(f'H(A,B) = {entropy(p)}')
+    ha = entropy(p, 1)
+    hb = entropy(p, 0)
+    hab = entropy(p)
+    ha_b = conditional_entropy(p, 0)
+    hb_a = conditional_entropy(p, 1)
+    print(f'H(A) = {ha}')
+    print(f'H(B) = {hb}')
+    print(f'H(A,B) = {hab}')
+    print(f'H(A|B) = {ha_b}')
+    print(f'H(B|A) = {hb_a}')
+    print(f'H(A,B) = {hab} = {ha + hb_a} = {hb + ha_b}')
+    print(f'I(A,B) = I(B,A) = {ha - ha_b} = {hb - hb_a}')
 
 
 def entropy(p: ndarray, axis: Optional[int] = None) -> float:
@@ -26,9 +35,22 @@ def entropy(p: ndarray, axis: Optional[int] = None) -> float:
     axis == 0, -> H(B)
     axis is None, -> H(A,B)
     """
-    total_p = p if axis is None else sum(p, axis=axis)
-    no_zeros = where(total_p == 0.0, 1.0, total_p)
-    return sum(-no_zeros * log2(no_zeros))  # noqa
+    return sum(base_entropy(p if axis is None else sum(p, axis=axis)))  # noqa
+
+
+def base_entropy(p: ndarray) -> ndarray:
+    no_zeros = where(p == 0.0, 1.0, p)
+    return -no_zeros * log2(no_zeros)
+
+
+def conditional_entropy(p: ndarray, axis: int) -> float:
+    """
+    axis == 1, -> P(B|A)
+    axis == 0, -> P(A|B)
+    """
+    return sum(
+        sum(base_entropy(p / sum(p, axis=axis)), axis=axis) * sum(p, axis=axis)
+    )  # noqa
 
 
 if __name__ == '__main__':
